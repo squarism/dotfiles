1e97e40af8b56d0251a42b3ae2870643fd84ae49
Chris Dillon <squarism@gmail.com>
2020-05-18 10:39:48 -0700

    Create abbreviations.fish

---
+++ fish/abbreviations.fish
@@ -0,0 +1,23 @@
+abbr -a -U -- ... 'cd ../..'
+abbr -a -U -- b bundle
+abbr -a -U -- be 'bundle exec'
+abbr -a -U -- c 'code .'
+abbr -a -U -- dcu 'docker-compose up'
+abbr -a -U -- ec 'iex -S mix'
+abbr -a -U -- g git
+abbr -a -U -- gca 'git commit -a -m'
+abbr -a -U -- glg 'git log --graph --oneline --decorate --color'
+abbr -a -U -- gpr 'git pull --rebase --prune'
+abbr -a -U -- gst 'git status'
+abbr -a -U -- jw 'npx jest --watch'
+abbr -a -U -- listening 'lsof -Pn -i | grep LISTEN'
+abbr -a -U -- locate mdfind
+abbr -a -U -- ls exa
+abbr -a -U -- pr 'poetry run'
+abbr -a -U -- redis redis-cli
+abbr -a -U -- sanity 'bundle exec rspec; if test $status = 0; bundle exec rubocop; end'
+abbr -a -U -- t 'exa -l -T -L 2 --header --git-ignore -F -d -I node_modules'
+abbr -a -U -- telnet 'nc -vz'
+abbr -a -U -- uuidgen uuidgen\ \|\ tr\ \'\[:upper:\]\'\ \'\[:lower:\]\'
+abbr -a -U -- vi nvim
+abbr -a -U -- vim nvim
